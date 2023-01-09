d133bbc8c64bfd4887f3e8c3dfed09fe77ae4c39
Rayane KADRI <kadrirayane@gmail.com>
2023-01-09 23:53:47 +0100

    Projet de test complet

---
+++ app.js
@@ -0,0 +1,5 @@
+const bup = require('bupmyfiles');
+
+bup.sauvegarde('./origine','./destination');
+//bup.sauvegardeMinuteur('./origine','./destination');
+//bup.restaure('./destination/BUP-2023-01-09.tar.gz','./restauration');
